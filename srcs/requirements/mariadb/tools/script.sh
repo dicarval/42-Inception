bc70aa2f23d5db4c59b2c82d191710b0a5dd8507
dicarval <diogo.c.santos10@gmail.com>
2025-08-01 17:21:02 +0100

    Figuring out the mariaDB specs

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+DOMAIN="$USER$DOMAIN_SUFFIX"
+
+# Starting and configuring Mariadb #
+echo "Configuring MariaDB ..."
+service mariadb start > /dev/null
+mysql -e "CREATE DATABASE IF NOT EXISTS \`$DOMAIN\` ;"
+mysql -e "CREATE USER IF NOT EXISTS '$USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD' ;"
+mysql -e "GRANT ALL PRIVILEGES ON \`$DOMAIN\`.* TO '$USER'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
+mysql -e "FLUSH PRIVILEGES ;"
+
+# Reseting Mariadb so the changes take effect #
+sleep 2
+service mariadb stop > /dev/null
+echo "MariaDB is ready!"
+exec mysqld_safe --bind-address=0.0.0.0 > /dev/null
