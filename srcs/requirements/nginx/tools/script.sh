345971855c46d8fe8f4b03662a2f8b5eccdced8e
dicarval <diogo.c.santos10@gmail.com>
2025-08-01 17:05:57 +0100

    Figuring out the mariaDB specs

---
+++ srcs/requirements/nginx/tools/script.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+DOMAIN="dicarval.42.fr"
+
+if [ -f /nginx_server.conf ]; then
+
+  # Generating a self signing certificate and a private key with OpenSSL #
+  echo "Generating key and certificate (OpenSSL) ..."
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout /etc/ssl/private/$DOMAIN.key \
+  -out /etc/ssl/certs/$DOMAIN.crt \
+  -subj "/C=PT/L=Lisbon/O=42Lisboa/OU=student/CN=$DOMAIN/UID=$USER" > /dev/null 2>&1
+
+  # Configuring nginx #
+  echo "Configuring Nginx ..."
+  sed -i "s/domain/$DOMAIN/g" nginx_server.conf
+  mv nginx_server.conf /etc/nginx/$DOMAIN.conf
+  mkdir -p /run/nginx
+
+fi
+
+echo "Nginx is ready!"
+nginx -g "daemon off;"
