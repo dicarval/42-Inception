cf5c0f66010a1ae0a905f1e1f446bba48fdff22a
Diogo Alexandre Carvalho Santos <dicarval@c2r6s13.42lisboa.com>
2025-08-14 15:20:51 +0100

    Debbugging MariaDB connection

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -3,6 +3,8 @@ DOMAIN="$USER$DOMAIN_SUFFIX"
 DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password.txt)
 DB_USER_PASSWORD=$(cat /run/secrets/db_user_password.txt)
 
+DB_NAME="$USER""_wordpress"
+
 # Starting and configuring MariaDB #
 echo "Configuring MariaDB ..."
 
@@ -21,11 +23,9 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	done
 
 	# Configure database and user
-	mariadb -e "CREATE DATABASE IF NOT EXISTS \`$DOMAIN\` ;"
+	mariadb -e "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\` ;"
 	mariadb -e "CREATE USER IF NOT EXISTS '$USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD' ;"
-	mariadb -e "GRANT ALL PRIVILEGES ON \`$DOMAIN\`.* TO '$USER'@'%' ;"
-	#mariadb -e "CREATE USER IF NOT EXISTS '$USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD' ;"
-	#mariadb -e "GRANT ALL PRIVILEGES ON \`$DOMAIN\`.* TO '$USER'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
+	mariadb -e "GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$USER'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
 	mariadb -e "FLUSH PRIVILEGES ;"
 
 	# Graceful shutdown
